bc71a30580e14ef61191cfce218d5b2f0a406523
Somchai Somphadung <somchaisomph@gmail.com>
2024-06-13 07:27:17 +0700

    Update pyBLE.py

--- pyBLE.py
+++ pyBLE.py
@@ -84,31 +84,3 @@ class BLEDevice():
 			print(f"An exception occured : {error}")		
 			
 # ---------------------------------------------------------------		
-
-
-def callback(val):
-	#print(val)
-	t,h,m,p= val.decode('utf-8').split(";")
-	print(f'TempC : {t} C')	
-	print(f'Humidity : {h} %')	
-	print(f'Soil Moisture : {m} %')	
-	print("----------")
-
-if __name__ == "__main__":
-	grass = "28:CD:xx:xx:xx:xx"
-	vetgy = "28:CD:xx:xx:xx:xx"
-	ble = BLEDevice()
-	ble.start()
-	ble.connect(vetgy)
-	if ble.isConected():
-		ble.readHnd(9,callback)
-		ble.writeHnd(int('c',16),'P1')
-		time.sleep(5)
-		ble.writeHnd(int('c',16),'P0')
-		time.sleep(2)
-		ble.readHnd(9,callback)
-		time.sleep(2)
-		ble.readHnd(9,callback)
-		ble.disconnect()
-		ble.stop()
-		
